eb90fbf6c34a46aefaa365510b9da69c820636ae
LeonardoLB <leonardolsbufalo@gmail.com>
2020-07-17 14:36:02 -0300

    service annotation

--- rabbit-producer/src/main/java/com/leonardobufalo/rabbitproducer/service/implementation/AmqpServiceImpl.java
+++ rabbit-producer/src/main/java/com/leonardobufalo/rabbitproducer/service/implementation/AmqpServiceImpl.java
@@ -4,7 +4,9 @@ import com.leonardobufalo.rabbitproducer.amqp.AmqpProducer;
 import com.leonardobufalo.rabbitproducer.dto.Message;
 import com.leonardobufalo.rabbitproducer.service.AmqpService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AmqpServiceImpl implements AmqpService {
 
     @Autowired
